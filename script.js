d1c86439d18c16c18e889ccb43bd65536f04278e
kazikaif <130026107+kazikaif@users.noreply.github.com>
2025-02-06 12:24:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function firstWord(s) {
   // your code here
 	let str = s.trim();
-	 return str.split(" ");
+	  str.split(" ");
 	
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord());
